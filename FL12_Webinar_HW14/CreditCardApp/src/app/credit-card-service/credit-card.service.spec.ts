c60af910de4914bdf96982a5bdeaa618580a9101
Tanya Gurova <tgurova@star.global>
2020-04-06 22:27:42 +0300

    hw14 tbd

---
+++ FL12_Webinar_HW14/CreditCardApp/src/app/credit-card-service/credit-card.service.spec.ts
@@ -0,0 +1,18 @@
+import { TestBed } from '@angular/core/testing';
+
+import { CreditCardService } from './credit-card.service';
+
+describe('CreditCardService', () => {
+  let service: CreditCardService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(CreditCardService);
+  });
+
+  it('should create CreditCardService', async () => {
+    expect(service).toBeTruthy();
+  });
+
+  
+});
